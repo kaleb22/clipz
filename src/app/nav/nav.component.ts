bcb9e5a45ed6e499f9e0372b09d3b8e6408e0a5b
kaleb22 <kaleb22@hotmail.com>
2022-06-08 19:19:54 -0300

    feat: impl action for opening the modal

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public modal: ModalService) { }
 
   ngOnInit(): void {
+  }
+
+  openModal($event: Event) {
+    $event.preventDefault(); // it will not allow the anchor to be executed (href)
+
+    this.modal.toogleModal();
   }
 
 }
