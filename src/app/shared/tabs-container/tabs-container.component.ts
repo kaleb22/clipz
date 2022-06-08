9650bd861053990dea7b2f383df596dbd66b5fb0
kaleb22 <kaleb22@hotmail.com>
2022-06-08 20:28:42 -0300

    feat: adding type to contentchildren decorator

--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
 
 @Component({
   selector: 'app-tabs-container',
   templateUrl: './tabs-container.component.html',
   styleUrls: ['./tabs-container.component.scss']
 })
-export class TabsContainerComponent implements OnInit {
+export class TabsContainerComponent implements AfterContentInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  @ContentChildren(TabComponent) tabs: QueryList<TabComponent> = new QueryList();
+
+  ngAfterContentInit(): void {
+    console.log(this.tabs)
   }
 
 }
