99f9050df9f67cd1c1c5b58526bda0556031c495
kaleb22 <kaleb22@hotmail.com>
2022-06-09 18:54:38 -0300

    feat: impl input component and handling validation errors

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,16 +7,22 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ]);
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  confirmPassword = new FormControl('');
+  phoneNumber = new FormControl('')
 
   registerForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3)
-    ]),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    phoneNumber: new FormControl('')
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber
   });
 }
