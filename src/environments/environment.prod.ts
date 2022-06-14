e0c901d1b4e50aec9e97baa2c1409deab53d74d1
kaleb22 <kaleb22@hotmail.com>
2022-06-14 18:59:58 -0300

    feat: adding firebase to project and configuring it

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBFX17o0EcMY3-kLODWZQg29D5sPs15w3k",
+    authDomain: "clipz-61ff3.firebaseapp.com",
+    projectId: "clipz-61ff3",
+    storageBucket: "clipz-61ff3.appspot.com",
+    messagingSenderId: "699211384744",
+    appId: "1:699211384744:web:dfd90e1770db04e7a7bf9d"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBFX17o0EcMY3-kLODWZQg29D5sPs15w3k",
+    authDomain: "clipz-61ff3.firebaseapp.com",
+    projectId: "clipz-61ff3",
+    storageBucket: "clipz-61ff3.appspot.com",
+    messagingSenderId: "699211384744",
+    appId: "1:699211384744:web:dfd90e1770db04e7a7bf9d"
+  }
 };
 
 /*
