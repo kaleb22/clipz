ecdc1c34dfb7394443b1a445e0efce79b40b28a0
kaleb22 <kaleb22@hotmail.com>
2022-06-21 17:55:04 -0300

    feat: hinding upload form

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -10,12 +10,22 @@ export class UploadComponent implements OnInit {
   constructor() { }
 
   isDragOver = false;
+  file: File | null = null;
+  hideForm = true;
 
   ngOnInit(): void {
   }
 
   storeFile($event: Event) {
     this.isDragOver = false;
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null; // ?? - nullish operator. if undefined it will return null
+
+    if(!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+
+    this.hideForm = false;
   }
 
 }
