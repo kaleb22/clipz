a6f6d153e3bc5ea391a86231540a4d42bc4bd524
kaleb22 <kaleb22@hotmail.com>
2022-07-29 15:28:59 -0300

    feat: refactor to generate more screen shoots

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -28,15 +28,30 @@ export class FfmpegService {
 
     this.ffmpeg.FS('writeFile', file.name, data);
 
+    const seconds = [1, 2, 3];
+    const commands: string[] = []
+
+    seconds.forEach(second => {
+      commands.push(
+      '-i', file.name,
+      '-ss', `00:00:0${second}`,
+      '-frames:v', '1',
+      '-filter:v', 'scale=510:-1',
+      `output_0${second}.png`
+      )
+    })
+
     await this.ffmpeg.run(
-      // input
-      '-i', file.name, // process the file
-      // output options
-      '-ss', '00:00:01', // get frame from time stamp
-      '-frames:v', '1', // generate 1 frame
-      '-filter:v', 'scale=510:-1', // maintain the aspect-ratio of file
-      // output
-      'output_01.png' // generate the image with this name
+      ...commands
     );
   }
+
+    // input
+    //'-i', file.name, // process the file
+    // output options
+    //'-ss', '00:00:01', // get frame from time stamp
+    //'-frames:v', '1', // generate 1 frame
+    //'-filter:v', 'scale=510:-1', // maintain the aspect-ratio of file
+    // output
+    //'output_01.png' // generate the image with this name
 }
