8c84f394c1eb0eb9ea91e9c774d952a4982c38ce
kaleb22 <kaleb22@hotmail.com>
2022-07-29 16:23:26 -0300

    feat: uploading img to firebase

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -64,6 +64,14 @@ export class FfmpegService {
     return screnshots;
   }
 
+  async blobFromURL(url: string) {
+    const response = await fetch(url);
+
+    const blob = await response.blob();
+
+    return blob;
+  }
+
     // input
     //'-i', file.name, // process the file
     // output options
