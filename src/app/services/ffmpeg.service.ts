5ae030c42409212eea8e8800f8f1387226414e32
kaleb22 <kaleb22@hotmail.com>
2022-07-29 15:22:11 -0300

    feat: generating one image screen shoot from video uploaded

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -27,5 +27,16 @@ export class FfmpegService {
     const data = await fetchFile(file);
 
     this.ffmpeg.FS('writeFile', file.name, data);
+
+    await this.ffmpeg.run(
+      // input
+      '-i', file.name, // process the file
+      // output options
+      '-ss', '00:00:01', // get frame from time stamp
+      '-frames:v', '1', // generate 1 frame
+      '-filter:v', 'scale=510:-1', // maintain the aspect-ratio of file
+      // output
+      'output_01.png' // generate the image with this name
+    );
   }
 }
