de42e0310223ce3756beb5c50455cccb6d82c2b6
kaleb22 <kaleb22@hotmail.com>
2022-07-30 00:55:53 -0300

    feat: adding videojs to project and importing styles

--- src/app/clip/clip.component.ts
+++ src/app/clip/clip.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import videojs from 'video.js';
 
 @Component({
   selector: 'app-clip',
   templateUrl: './clip.component.html',
-  styleUrls: ['./clip.component.scss']
+  styleUrls: ['./clip.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ClipComponent implements OnInit {
 
   id = '';
+  player?: videojs.Player;
+  
+  @ViewChild('videoPlayer', { static: true }) target?: ElementRef;
 
   constructor(public route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.player = videojs(this.target?.nativeElement);
+
     this.route.params.subscribe( (params: Params) => {
       this.id = params.id;
     })
