c8245157550c2d739ed17f9e074a7a841f80ca05
kaleb22 <kaleb22@hotmail.com>
2022-07-30 01:28:43 -0300

    feat: func to clipboard url

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -79,4 +79,18 @@ export class ManageComponent implements OnInit {
     })
   }
 
+  async copyToClipboard($event: MouseEvent, docId: string | undefined) {
+    $event.preventDefault();
+
+    if(!docId) {
+      return;
+    }
+
+    const url = `${location.origin}/clip/${docId}`;
+
+    await navigator.clipboard.writeText(url);
+
+    alert('link copied!');
+  }
+
 }
